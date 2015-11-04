2fa37bac69160d882e0e17ad14d9be53787f25ca
Dominik Lubański <dominik.lubanski@gmail.com>
2015-11-04 15:42:25 +0100

    Polyfills removed, bower dependencies

--- linked-html.js
+++ linked-html.js
@@ -1,7 +1,3 @@
-// ES6 polyfills
-import 'core-js/modules/es6.object.assign';
-import 'core-js/modules/es6.array.from';
-
 // Re-exports
 export { default as Engine } from './src/Engine';
 export { default as Expression } from './src/Expression/Expression';
